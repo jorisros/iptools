7d7cc2bf61fc42e23e574ce1540d9021986d1c92
Joris Ros <joris.ros@gmail.com>
2018-08-04 23:42:31 +0200

    Add logic for see if ip is in range

--- src/IpTools.php
+++ src/IpTools.php
@@ -1,10 +1,14 @@
 <?php
 
-namespace JorisRos\IpTools;
+namespace JorisRos;
 
 class IpTools
 {
-    public static function validateIp (string $ipaddress) : bool
+    const SEPARATION_METHOD_WILDCARD = '*';
+    const SEPARATION_METHOD_RANGE = '-';
+    const SEPARATION_METHOD_NULL = null;
+
+    public static function validateIp(string $ipaddress) : bool
     {
         if (filter_var($ipaddress, FILTER_VALIDATE_IP)) {
             return true;
@@ -13,7 +17,7 @@ class IpTools
         }
     }
 
-    public static function isIpInRange ($ip, $range) : bool
+    public static function isIpInRange($ip, $range) : bool
     {
         if (!self::validateIp($ip)) {
             return false;
@@ -21,10 +25,57 @@ class IpTools
 
         $range = self::determRange($range);
 
+        if (self::validateIp($range['low']) && self::validateIp($range['high'])) {
+            if (ip2long($range['low']) < ip2long($ip) && ip2long($range['high']) > ip2long($ip)) {
+                return true;
+            }
+        } else {
+            return false;
+        }
+        return false;
     }
 
-    public static function determRange ($range) : array
+    public static function determRange($range) : array
     {
+        $range = self::stripAndTrimSpaces($range);
 
+        switch (self::determSeperation($range)) {
+            case self::SEPARATION_METHOD_RANGE:
+                $arr = explode(self::SEPARATION_METHOD_RANGE, $range);
+
+                return [
+                    'low' => $arr[0],
+                    'high' => $arr[1]
+                ];
+                break;
+            case self::SEPARATION_METHOD_NULL:
+            default:
+                break;
+        }
+        return [];
+    }
+
+    public static function stripAndTrimSpaces(string $string) : string
+    {
+        return trim(str_replace(' ', '', $string));
+    }
+
+    public static function determSeperation($string) : string
+    {
+        $pos = strpos($string, self::SEPARATION_METHOD_RANGE);
+
+        if ($pos === false) {
+            return self::SEPARATION_METHOD_NULL;
+        } else {
+            return self::SEPARATION_METHOD_RANGE;
+        }
+
+        $pos = strpos($string, self::SEPARATION_METHOD_WILDCARD);
+
+        if ($pos === false) {
+            return self::SEPARATION_METHOD_NULL;
+        } else {
+            return self::SEPARATION_METHOD_WILDCARD;
+        }
     }
 }
